6d3981525e4cca9486c794cf0264e7c5d88e8c3a
ProemPhearom <PhearomProem@gmail.com>
2022-06-22 01:23:57 +0700

    Add all my files laravel9

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    	body{
+    		background: #f5fbf5;
+    		padding: 50px;
+    	}
+    </style>
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
